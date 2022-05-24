2b4902ae3479dd30cacd6a6ea3f631962db8b59c
supriyasethi <60409351+supriyasethi@users.noreply.github.com>
2022-05-24 01:01:57 -0700

    First Commit

---
+++ src/main/java/io/supriya/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.supriya.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/supriya/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,8 @@
+package io.supriya.coronavirustracker.services;
+
+public class CoronaVirusDataService {
+
+    public void fetchVirusData(){
+
+    }
+}
